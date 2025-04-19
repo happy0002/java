150db831774f17ee2eab3f725361645d47bf6f1d
{Happy <{happyswaraj7667@gmail.com}>
2025-04-19 23:38:21 +0530

    taking input from user

---
+++ input.java
@@ -0,0 +1,12 @@
+// taking input from user...
+import java.util.Scanner;
+
+public class input {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your name :");
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+}
